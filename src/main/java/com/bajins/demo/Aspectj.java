ba866796ae3aea69c53f306854b4c7058a0aa2f3
woytu <394585829@qq.com>
2020-05-02 00:04:43 +0800

    update 通过切点获取解析参数

--- src/main/java/com/bajins/demo/Aspectj.java
+++ src/main/java/com/bajins/demo/Aspectj.java
@@ -1,9 +1,9 @@
 package com.bajins.demo;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.aspectj.lang.JoinPoint;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.aspectj.lang.reflect.MethodSignature;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.annotation.Annotation;
@@ -22,15 +22,16 @@ import java.util.Map;
  */
 public class Aspectj {
 
+
     /**
      * 获取注解中对方法的描述信息
      *
-     * @param joinPoint       切点
-     * @param annotationClass 被使用注解的类
+     * @param joinPoint 切点
+     * @param clazz     被使用注解的类
      * @return
      * @throws Exception
      */
-    public static <T extends Annotation> T getMethodAnnotate(JoinPoint joinPoint, Class<T> annotationClass) throws Exception {
+    public static <T extends Annotation> T getMethodAnnotate(JoinPoint joinPoint, Class<T> clazz) throws ClassNotFoundException {
         // 获取目标类名
         String targetName = joinPoint.getTarget().getClass().getName();
         // 获取方法名
@@ -48,55 +49,136 @@ public class Aspectj {
             // 获取使用反射生成类对象中的所有参数类型,进行判断是否一致
             if (method.getName().equals(methodName) && method.getParameterTypes().length == arguments.length) {
                 // 获取使用反射生成类对象中使用自定义注解方法上的描述
-                annotation = method.getAnnotation(annotationClass);
+                annotation = method.getAnnotation(clazz);
             }
+            /**
+             * 返回 Class 对象所表示的类或接口所声明的所有字段，
+             * 包括公共、保护、默认（包）访问和私有字段，但不包括继承的字段。
+             */
+            /*Field[] declaredFields = clazz.getDeclaredFields();
+            for (int i = 0; i < declaredFields.length; i++) {
+                String attributeName = declaredFields[i].getName();
+                System.out.println(attributeName);
+            }*/
         }
-        /**
-         * 返回 Class 对象所表示的类或接口所声明的所有字段，
-         * 包括公共、保护、默认（包）访问和私有字段，但不包括继承的字段。
-         */
-        /*Field[] declaredFields = annotationClass.getDeclaredFields();
-        for (int i = 0; i < declaredFields.length; i++) {
-            String attributeName = declaredFields[i].getName();
-            System.out.println(attributeName);
-        }*/
         return annotation;
     }
 
     /**
-     * 获取切点的参数，根据请求方法使用不同的方式获取参数
+     * 获取切点位置的参数
      *
      * @param joinPoint 切点
-     * @param request
+     * @param request   从切面传入的当前HttpServletRequest
      * @return java.lang.String
      */
-    public static String getParam(JoinPoint joinPoint, HttpServletRequest request) throws JsonProcessingException {
-        String method = request.getMethod();
+    public static String getParam(JoinPoint joinPoint, HttpServletRequest request) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        // 获取切点位置的方法上的参数名
+        String[] parameterNames = methodSignature.getParameterNames();
+        // 获取切点位置的方法上的参数值
+        Object[] args = joinPoint.getArgs();
 
-        // 判断请求方式是否为POST
-        if (RequestMethod.POST.toString().equals(method)) {
-            Object[] args = joinPoint.getArgs();
-            if (args.length <= 0) {
-                return "";
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        String parameterName;
+        Object arg;
+        for (int i = 0; i < parameterNames.length; i++) {
+            parameterName = parameterNames[i];
+            arg = args[i];
+            if (arg instanceof HttpServletRequest) {
+                // 获取HttpServletRequest中的请求标准参数
+                Map<String, String[]> properties = request.getParameterMap();
+                Map<String, String> returnMap = new HashMap<>();
+                for (Map.Entry<String, String[]> entry : properties.entrySet()) {
+                    returnMap.put(entry.getKey(), String.join(",", entry.getValue()));
+                }
+                if (returnMap != null && returnMap.size() > 0) {
+                    objectNode.putPOJO(parameterName, returnMap);
+                }
+            } else {
+                objectNode.putPOJO(parameterName, arg);
             }
-            // 如果切点拿到的第一个参数是HttpServletRequest并且不为空
-            if (args[0] instanceof HttpServletRequest && args[0] != null) {
-                // POST请求方式的真正HttpServletRequest在joinPoint.getArgs()[0]中
-                request = (HttpServletRequest) args[0];
-            }
         }
-        // 判断HttpServletRequest是否为空
-        if (request == null) {
-            return "";
-        }
-        // 获取HttpServletRequest中的参数map
-        Map<String, String[]> properties = request.getParameterMap();
-        Map<String, String> returnMap = new HashMap<String, String>();
-        for (Map.Entry<String, String[]> entry : properties.entrySet()) {
-            returnMap.put(entry.getKey(), String.join(",",entry.getValue()));
-        }
-        return new ObjectMapper().writeValueAsString(returnMap);
+        return objectNode.toString();
     }
+
+    /**
+     * 获取切点位置的参数
+     *
+     * @param joinPoint 切点
+     * @return
+     */
+    public static String getParam(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        // 获取切点位置的方法上的参数名
+        String[] parameterNames = methodSignature.getParameterNames();
+        // 获取切点位置的方法上的参数值
+        Object[] args = joinPoint.getArgs();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        String parameterName;
+        Object arg;
+        for (int i = 0; i < parameterNames.length; i++) {
+            parameterName = parameterNames[i];
+            arg = args[i];
+            if (arg instanceof HttpServletRequest) {
+                HttpServletRequest request = (HttpServletRequest) arg;
+                // 获取HttpServletRequest中的请求标准参数
+                Map<String, String[]> properties = request.getParameterMap();
+                Map<String, String> returnMap = new HashMap<>();
+                for (Map.Entry<String, String[]> entry : properties.entrySet()) {
+                    returnMap.put(entry.getKey(), String.join(",", entry.getValue()));
+                }
+                if (returnMap != null && returnMap.size() > 0) {
+                    objectNode.putPOJO(parameterName, returnMap);
+                }
+            } else {
+                objectNode.putPOJO(parameterName, arg);
+            }
+        }
+        return objectNode.toString();
+    }
+
+    /**
+     * 借助了 Mybatis 通过切点获取方法上的参数名
+     *
+     * @param joinPoint
+     * @return
+     * @throws NotFoundException
+     */
+    /*private Map<String, Object> getFieldsName(JoinPoint joinPoint) throws NotFoundException {
+        Map<String, Object> map = new HashMap<>();
+        // 获取切点位置的类
+        Class<?> aClass = joinPoint.getTarget().getClass();
+        // 获取类对象表示的实体名称
+        String clazzName = aClass.getName();
+        // 获取切点位置的方法名称
+        String methodName = joinPoint.getSignature().getName();
+        // 获取切点位置的方法上的参数值
+        Object[] args = joinPoint.getArgs();
+
+        // 类池子
+        ClassPool pool = ClassPool.getDefault();
+        // 获取类路径
+        ClassClassPath classPath = new ClassClassPath(aClass);
+        // 装入类路径
+        pool.insertClassPath(classPath);
+        // 从池子中获取类
+        CtClass cc = pool.get(clazzName);
+        CtMethod cm = cc.getDeclaredMethod(methodName);
+        // 获取方法信息
+        MethodInfo methodInfo = cm.getMethodInfo();
+        // 获取方法上的参数名
+        CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
+        LocalVariableAttribute attr = (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag);
+        // 判断是否为静态
+        int pos = Modifier.isStatic(cm.getModifiers()) ? 0 : 1;
+        for (int i = 0; i < cm.getParameterTypes().length; i++) {
+            map.put(attr.variableName(i + pos), args[i]);
+        }
+        return map;
+    }*/
 
     /**
      * 获取 类.方法
