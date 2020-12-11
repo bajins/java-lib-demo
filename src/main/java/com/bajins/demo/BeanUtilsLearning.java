97fc277499c4f695ceb1ee96b823c24d4e80000c
woetu <admin@woetu.com>
2020-12-11 20:24:03 +0800

    update

--- src/main/java/com/bajins/demo/BeanUtilsLearning.java
+++ src/main/java/com/bajins/demo/BeanUtilsLearning.java
@@ -14,7 +14,10 @@ public class BeanUtilsLearning {
             /**
              * 对象拷贝
              */
+            // No value specified for 'Date'：复制对象内存在Date类型的属性，但是Date没有初始值，因此需要提前设置初始值
+            //ConvertUtils.register(new DateConverter(null), java.util.Date.class);
             //org.apache.commons.beanutils.BeanUtils.copyProperties();
+
             //org.apache.commons.beanutils.PropertyUtils.copyProperties();
             //org.springframework.beans.BeanUtils.copyProperties();
             //org.springframework.cglib.beans.BeanCopier.create().copy();
