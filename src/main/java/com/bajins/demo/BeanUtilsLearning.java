9ce1e141fb1fcaaec6e48e39e6beacde78a4f73e
woetu <admin@woetu.com>
2020-12-12 11:09:27 +0800

    add

--- src/main/java/com/bajins/demo/BeanUtilsLearning.java
+++ src/main/java/com/bajins/demo/BeanUtilsLearning.java
@@ -1,10 +1,48 @@
 package com.bajins.demo;
 
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.beans.FeatureDescriptor;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class BeanUtilsLearning {
+
+    /**
+     * 获取所有非空的属性
+     *
+     * @param source
+     * @return
+     */
+    public static String[] getPropertyNamesNotNull(Object source) {
+        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = wrappedSource.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = wrappedSource.getPropertyValue(pd.getName());
+            if (null != srcValue) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public static String[] getPropertyNamesNotNullByStream(Object source) {
+        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
+        // Java8 Stream获取非空属性
+        String[] result = Stream.of(wrappedSource.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) != null)
+                .toArray(String[]::new);
+        return result;
+    }
 
     public static void main(String[] args) {
         try {
