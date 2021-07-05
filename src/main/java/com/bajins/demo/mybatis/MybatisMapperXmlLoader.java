108e15aaa7ff0d99b4a86d2c5e3ddfa415ee3cdb
woetu <admin@woetu.com>
2021-07-05 15:06:07 +0800

    update

---
+++ src/main/java/com/bajins/demo/mybatis/MybatisMapperXmlLoader.java
@@ -0,0 +1,171 @@
+package com.bajins.demo.mybatis;
+
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 动态刷新xml、热加载xml、热部署xml、动态加载xml、自动加载xml
+ * <p>
+ * 此类主要将项目启动时xml文件加载到 sqlSessionFactory 中的 Configuration缓存清除
+ * ,再扫描本地xml加入到该缓存中,从而达到,xml没有经过编译也可以实现实时更新
+ * ，网上也有其他的加载的编译后的xml,也就是build路径加的xml,但如果项目没有编译,xml无法实时更新
+ * ,注意scanMapperXml()方法的路径你要换成自己对应的XML文件路径
+ * <p>
+ * 手动刷新 new XMLMapperLoader(sqlSessionFactory,"/mapper").readMapperXml()
+ */
+public class MybatisMapperXmlLoader {
+
+    private static final Logger logger = LoggerFactory.getLogger(MybatisMapperXmlLoader.class);
+
+    private SqlSessionFactory sqlSessionFactory;
+    private Resource[] mapperLocations;
+    private String packageSearchPath = "/mapper";
+
+    public MybatisMapperXmlLoader(SqlSessionFactory sqlSessionFactory, String packageSearchPath) {
+        this.sqlSessionFactory = sqlSessionFactory;
+        if (StringUtils.hasText(packageSearchPath)) {
+            this.packageSearchPath = packageSearchPath;
+        }
+        startThreadListener();
+    }
+
+    /**
+     * 自动刷新
+     */
+    public void startThreadListener() {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+        //每10秒执行一次
+        service.scheduleAtFixedRate(() -> readMapperXml(), 0, 10, TimeUnit.SECONDS);
+        readMapperXml();
+    }
+
+    /**
+     * 加载xml
+     *
+     * @return
+     */
+    public String readMapperXml() {
+        try {
+            org.apache.ibatis.session.Configuration configuration = sqlSessionFactory.getConfiguration();
+            // 扫描文件
+            this.scanMapperXml();
+
+            if (true) {
+                // 清空configuration map的数据
+                this.removeConfig(configuration);
+
+                // 将xml 重新加载
+                for (Resource configLocation : mapperLocations) {
+                    if ("TestMapper.xml".equals(configLocation.getFilename())) {
+                        try {
+                            XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(configLocation.getInputStream(),
+                                    configuration, configLocation.toString(), configuration.getSqlFragments());
+                            xmlMapperBuilder.parse();
+                            logger.debug("mapper文件[" + configLocation.getFilename() + "]缓存加载成功");
+                        } catch (IOException e) {
+                            logger.debug("mapper文件[" + configLocation.getFilename() + "]不存在或内容格式不对");
+                            continue;
+                        }
+                    }
+                }
+            }
+            return "refresh mybatis xml successful";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "refresh mybatis xml fail";
+        }
+    }
+
+
+    /**
+     * 扫描xml文件所在的路径
+     *
+     * @throws IOException
+     */
+    private void scanMapperXml() {
+        // 根据自己项目的实际路径查替换,最终是找到非编译的 xml所在的文件夹路径
+        String fileUrl = this.getClass().getResource(packageSearchPath).getPath().replace("/main", "").replace("build"
+                , "src/main");
+        File file = new File(fileUrl);
+        File[] matchingFiles = file.listFiles();
+        Set<Resource> result = new LinkedHashSet<>(matchingFiles.length);
+        for (File files : matchingFiles) {
+            result.add(new FileSystemResource(files));
+        }
+        this.mapperLocations = result.toArray(new Resource[0]);
+    }
+
+    /**
+     * 清空Configuration中几个重要的缓存
+     *
+     * @param configuration
+     * @throws Exception
+     */
+    private void removeConfig(Configuration configuration) throws Exception {
+        Class<?> classConfig = configuration.getClass();
+        clearMap(classConfig, configuration, "mappedStatements");
+        clearMap(classConfig, configuration, "caches");
+        clearMap(classConfig, configuration, "resultMaps");
+        clearMap(classConfig, configuration, "parameterMaps");
+        clearMap(classConfig, configuration, "keyGenerators");
+        clearMap(classConfig, configuration, "sqlFragments");
+        clearSet(classConfig, configuration, "loadedResources");
+    }
+
+    /**
+     * 删除xml元素的节点缓存
+     *
+     * @param classConfig
+     * @param configuration
+     * @param fieldName
+     * @throws Exception
+     */
+    private void clearMap(Class<?> classConfig, Configuration configuration, String fieldName) throws Exception {
+        Field field;
+        if (configuration.getClass().getName().equals("com.bajins.com.MybatisConfiguration")) {
+            field = classConfig.getSuperclass().getDeclaredField(fieldName);
+        } else {
+            field = classConfig.getClass().getDeclaredField(fieldName);
+        }
+        field.setAccessible(true);
+        Map mapConfig = (Map) field.get(configuration);
+        mapConfig.clear();
+    }
+
+
+    /**
+     * 清除文件记录缓存
+     *
+     * @param classConfig
+     * @param configuration
+     * @param fieldName
+     * @throws Exception
+     */
+    private void clearSet(Class<?> classConfig, Configuration configuration, String fieldName) throws Exception {
+        Field field;
+        if (configuration.getClass().getName().equals("com.bajins.com.MybatisConfiguration")) {
+            field = classConfig.getSuperclass().getDeclaredField(fieldName);
+        } else {
+            field = classConfig.getClass().getDeclaredField(fieldName);
+        }
+        field.setAccessible(true);
+        Set setConfig = (Set) field.get(configuration);
+        setConfig.clear();
+    }
+}
---
+++ src/main/java/com/bajins/demo/mybatis/MybatisMapperXmlRefresh.java
@@ -0,0 +1,163 @@
+package com.bajins.demo.mybatis;
+
+import com.google.common.collect.Lists;
+import org.apache.ibatis.binding.MapperRegistry;
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
+import org.apache.ibatis.builder.xml.XMLMapperEntityResolver;
+import org.apache.ibatis.executor.ErrorContext;
+import org.apache.ibatis.executor.keygen.SelectKeyGenerator;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.parsing.XNode;
+import org.apache.ibatis.parsing.XPathParser;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class MybatisMapperXmlRefresh {
+    private static final Logger logger = LoggerFactory.getLogger(MybatisMapperXmlRefresh.class);
+
+    private Configuration configuration;
+    private SqlSessionFactory sqlSessionFactory;
+
+    public MybatisMapperXmlRefresh(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    /**
+     * 刷新mapper
+     *
+     * @throws Exception
+     */
+    private void refresh(Resource resource)
+            throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        this.configuration = sqlSessionFactory.getConfiguration();
+        boolean isSupper = configuration.getClass().getSuperclass() == Configuration.class;
+        try {
+            Field loadedResourcesField = isSupper
+                    ? configuration.getClass().getSuperclass().getDeclaredField("loadedResources")
+                    : configuration.getClass().getDeclaredField("loadedResources");
+            loadedResourcesField.setAccessible(true);
+            Set loadedResourcesSet = ((Set) loadedResourcesField.get(configuration));
+            XPathParser xPathParser = new XPathParser(resource.getInputStream(), true, configuration.getVariables(),
+                    new XMLMapperEntityResolver());
+            XNode context = xPathParser.evalNode("/mapper");
+            String namespace = context.getStringAttribute("namespace");
+            Field field = MapperRegistry.class.getDeclaredField("knownMappers");
+            field.setAccessible(true);
+            Map mapConfig = (Map) field.get(configuration.getMapperRegistry());
+            Collection<String> mappedStatementNames = configuration.getMappedStatementNames();
+
+            mapConfig.remove(Resources.classForName(namespace));
+            loadedResourcesSet.remove(resource.toString());
+            configuration.getCacheNames().remove(namespace);
+
+            cleanParameterMap(context.evalNodes("/mapper/parameterMap"), namespace);
+            cleanResultMap(context.evalNodes("/mapper/resultMap"), namespace);
+            cleanKeyGenerators(context.evalNodes("insert|update|select"), namespace);
+            cleanSqlElement(context.evalNodes("/mapper/sql"), namespace);
+            XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(resource.getInputStream(),
+                    sqlSessionFactory.getConfiguration(), resource.toString(),
+                    sqlSessionFactory.getConfiguration().getSqlFragments());
+            xmlMapperBuilder.parse();
+            logger.debug("refresh: '" + resource + "', success!");
+        } catch (IOException e) {
+            logger.error("Refresh IOException :" + e.getMessage());
+        } finally {
+            ErrorContext.instance().reset();
+        }
+    }
+
+    /**
+     * 清理parameterMap
+     *
+     * @param list
+     * @param namespace
+     */
+    private void cleanParameterMap(List<XNode> list, String namespace) {
+        for (XNode parameterMapNode : list) {
+            String id = parameterMapNode.getStringAttribute("id");
+            configuration.getParameterMaps().remove(namespace + "." + id);
+        }
+    }
+
+    /**
+     * 清理resultMap
+     *
+     * @param list
+     * @param namespace
+     */
+    private void cleanResultMap(List<XNode> list, String namespace) {
+        for (XNode resultMapNode : list) {
+            String id = resultMapNode.getStringAttribute("id", resultMapNode.getValueBasedIdentifier());
+            configuration.getResultMapNames().remove(id);
+            configuration.getResultMapNames().remove(namespace + "." + id);
+            clearResultMap(resultMapNode, namespace);
+        }
+    }
+
+    private void clearResultMap(XNode xNode, String namespace) {
+        for (XNode resultChild : xNode.getChildren()) {
+            if ("association".equals(resultChild.getName()) || "collection".equals(resultChild.getName())
+                    || "case".equals(resultChild.getName())) {
+                if (resultChild.getStringAttribute("select") == null) {
+                    configuration.getResultMapNames()
+                            .remove(resultChild.getStringAttribute("id", resultChild.getValueBasedIdentifier()));
+                    configuration.getResultMapNames().remove(namespace + "."
+                            + resultChild.getStringAttribute("id", resultChild.getValueBasedIdentifier()));
+                    if (resultChild.getChildren() != null && !resultChild.getChildren().isEmpty()) {
+                        clearResultMap(resultChild, namespace);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 清理selectKey
+     *
+     * @param list
+     * @param namespace
+     */
+    private void cleanKeyGenerators(List<XNode> list, String namespace) {
+        for (XNode context : list) {
+            String id = context.getStringAttribute("id");
+            configuration.getKeyGeneratorNames().remove(id + SelectKeyGenerator.SELECT_KEY_SUFFIX);
+            configuration.getKeyGeneratorNames().remove(namespace + "." + id + SelectKeyGenerator.SELECT_KEY_SUFFIX);
+
+            Collection<MappedStatement> mappedStatements = configuration.getMappedStatements();
+            List<MappedStatement> objects = Lists.newArrayList();
+            Iterator<MappedStatement> it = mappedStatements.iterator();
+            while (it.hasNext()) {
+                Object object = it.next();
+                if (object instanceof org.apache.ibatis.mapping.MappedStatement) {
+                    MappedStatement mappedStatement = (MappedStatement) object;
+                    if (mappedStatement.getId().equals(namespace + "." + id)) {
+                        objects.add(mappedStatement);
+                    }
+                }
+            }
+            mappedStatements.removeAll(objects);
+        }
+    }
+
+    /**
+     * 清理sql节点缓存
+     *
+     * @param list
+     * @param namespace
+     */
+    private void cleanSqlElement(List<XNode> list, String namespace) {
+        for (XNode context : list) {
+            String id = context.getStringAttribute("id");
+            configuration.getSqlFragments().remove(id);
+            configuration.getSqlFragments().remove(namespace + "." + id);
+        }
+    }
+}
